5f1d0b147a369f3401f86347fdba14eda76adef4
venugopal devarapalli <venugopal.devarapalli@halliburton.com>
2018-07-08 22:08:29 -0500

    Added New files related to project.

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+});
+
+(function (global) {
+
+var dc = {};
+global.$dc = dc;
+
+///////////////////
+var width = 960
+var height = 600
+
+var svg = d3.select("#map")
+			.append("svg")
+			.attr("width", width)
+      .attr("height", height);
+      
+var path = d3.geoPath();
+
+d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+  if (error) throw error;
+
+  svg.append("g")
+      .attr("class", "states")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("path")
+      .attr("d", path);
+
+  svg.append("path")
+      .attr("class", "state-borders")
+      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+});
+
+//////////////
+
+
+})(window);
+
